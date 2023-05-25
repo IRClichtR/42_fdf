8ba55accf8ae3c830894b5c1f4f5a9a9a5d2a0b6
Floriane TUERNAL-SABOTINOV <ftuernal@e1r8p11.clusters.42paris.fr>
2023-05-25 13:00:26 +0200

    draw_line + libtools

--- SRCS/draw_line.c
+++ SRCS/draw_line.c
@@ -6,27 +6,27 @@
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 12:37:25 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/05/25 12:43:43 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/05/25 12:47:57 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_line(t_vector *coord, t_map *map)
+void	draw_line(t_vector *crd, t_map *map)
 {
 	float	dx;
 	float	dy;
 	int		*max;
 
-	dx = coord->x1 - coord->x;
-	dy = coord->y1 - coord->y;
+	dx = crd->x1 - crd->x;
+	dy = crd->y1 - crd->y;
 	max = define_max(define_absvalue(dx), define_absvalue(dy));
 	dx /= *max;
 	dy /= *max;
-	while ((coord->x - coord->x1) || (coord->y - coord->y1))
+	while ((crd->x - crd->x1) || (crd->y - crd->y1))
 	{
-		mlx_pixel_put(map->mlx_ptr, map->win_ptr, coord->x, coord->y, 0xffffff);
-		coord->x += dx;
-		coord->y += dy;
+		mlx_pixel_put(map->mlx_ptr, map->win_ptr, crd->x, crd->y, 0xffffff);
+		crd->x += dx;
+		crd->y += dy;
 	}
 }
